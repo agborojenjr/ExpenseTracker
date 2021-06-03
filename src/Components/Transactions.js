9960b3f876105c436a8ddd883c7496472e5c5e70
Agbor Ojen <ojenagborjnr@gmail.com>
2021-06-03 23:41:36 +0100

    History Title Added

--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -16,6 +17,9 @@ export const Transactions = () => {
 
     return (
     <form className={classes.root} noValidate autoComplete="off">
+        <Typography>
+            History
+        </Typography>
         <TextField id="outlined-basic" label="Cash" variant="outlined" />
         <TextField id="outlined-basic" label="Book" variant="outlined" />
         <TextField id="outlined-basic" label="Camera" variant="outlined" />
